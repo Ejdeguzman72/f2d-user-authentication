5a1a7be2914e513ffa915b7125c60d85ce2a78b4
Ejdeguzman72 <ejdeguzman72@gmail.com>
2024-09-16 23:50:41 -0400

    more changes

--- src/main/java/com/f2d/user_authentication/service/F2DUserDetailsService.java
+++ src/main/java/com/f2d/user_authentication/service/F2DUserDetailsService.java
@@ -2,6 +2,8 @@ package com.f2d.user_authentication.service;
 
 import com.f2d.user_authentication.domain.F2DUser;
 import com.f2d.user_authentication.repository.F2DUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,10 +15,13 @@ public class F2DUserDetailsService implements UserDetailsService {
 
     @Autowired
     private F2DUserRepository f2dUserRepository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(F2DUserDetailsService.class);
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         F2DUser user = f2dUserRepository.findByUsername(username);
+
+        LOGGER.info("Finding user: " + username);
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getAuthorities());
     }
